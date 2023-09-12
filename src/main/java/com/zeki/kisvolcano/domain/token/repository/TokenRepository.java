075c88f08715db7892d9407145b41581840fdd03
ojy9612 <ojy9612@gmail.com>
2023-09-12 16:23:10 +0900

    #3 domain refactor

--- src/main/java/com/zeki/kisvolcano/domain/token/repository/TokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zeki.kisvolcano.domain.token.repository;
-
-import com.zeki.kisvolcano.domain.token.entity.Token;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenRepository extends JpaRepository<Token,Long> {
-}
